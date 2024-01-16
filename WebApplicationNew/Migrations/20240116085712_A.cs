325dabbeee64b92ba5498e81439f09a622ae09ea
jksharma12796@gmail.com <jksharma12796@gmail.com>
2024-01-16 21:56:28 +0530

    Add project files.

---
+++ WebApplicationNew/Migrations/20240116085712_A.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplicationNew.Migrations
+{
+    /// <inheritdoc />
+    public partial class A : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    PetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Color = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Gender = table.Column<string>(type: "varchar(50)", nullable: false),
+                    OwnerName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Address = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.PetId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
